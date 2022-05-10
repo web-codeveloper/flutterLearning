accb85462f429901f8745fc7e804cb5d9b683a1b
parthamandal9 <parthamandal512@gmail.com>
2022-05-10 23:41:20 +0530

    Setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+ runApp(Center(
+   child: Text(
+     "Flutter course",
+     textDirection: TextDirection.ltr,
+     style: TextStyle(fontSize: 40.0),
+   ),
+ ));
+}
+

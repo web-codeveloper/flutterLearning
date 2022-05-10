9adb52ddde073c1ff39044b668581398123f2c9a
parthamandal9 <parthamandal512@gmail.com>
2022-05-11 00:04:23 +0530

    Use ListView,  height,  color

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,110 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    decoration: BoxDecoration(color: Colors.blueAccent),
-    child: Column(children: [
-      Text(
-        'Partha',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 60),
-      )
-    ]),
-  ));
+  runApp(Directionality(
+      textDirection: TextDirection.ltr,
+      child: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: [
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: Column(children: [
+              Text(
+                '1',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Color.fromARGB(255, 51, 255, 0),
+            child: Column(children: [
+              Text(
+                '2',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Color.fromARGB(255, 0, 4, 255),
+            child: Column(children: [
+              Text(
+                '3',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: Column(children: [
+              Text(
+                '4',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Color.fromARGB(255, 51, 255, 0),
+            child: Column(children: [
+              Text(
+                '5',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Color.fromARGB(255, 0, 4, 255),
+            child: Column(children: [
+              Text(
+                '6',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: Column(children: [
+              Text(
+                '7',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Color.fromARGB(255, 51, 255, 0),
+            child: Column(children: [
+              Text(
+                '8',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          ),
+          Container(
+            height: 100,
+            color: Color.fromARGB(255, 0, 4, 255),
+            child: Column(children: [
+              Text(
+                '9',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 60),
+              )
+            ]),
+          )
+        ],
+      )));
 }

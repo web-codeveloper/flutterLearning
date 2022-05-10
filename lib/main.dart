f383f14b6ed59d339237e9fcd82bd10d96286091
parthamandal9 <parthamandal512@gmail.com>
2022-05-10 23:47:24 +0530

    Use Column

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
- runApp(
-   Row(textDirection: TextDirection.ltr, children: [
-     Text(
-       'One',
-       textDirection: TextDirection.ltr,
-       style: TextStyle(fontSize: 50),
-     ),
-     Text(
-       'Two',
-       textDirection: TextDirection.ltr,
-       style: TextStyle(fontSize: 50),
-     ),
-     Text(
-       'Three',
-       textDirection: TextDirection.ltr,
-       style: TextStyle(fontSize: 50),
-     )
-   ]),
- );
+  runApp(
+    Column(textDirection: TextDirection.ltr, children: [
+      Text(
+        'One',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 50),
+      ),
+      Text(
+        'Two',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 50),
+      ),
+      Text(
+        'Three',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 50),
+      )
+    ]),
+  );
 }

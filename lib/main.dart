a5759663cd412943d00263eff73e57aeace7317e
parthamandal9 <parthamandal512@gmail.com>
2022-05-11 13:32:28 +0530

    Use GridView with extent

--- lib/main.dart
+++ lib/main.dart
@@ -15,5 +15,16 @@ void main() {
             backgroundColor: Colors.black87,
           ),
           backgroundColor: Colors.cyan,
-          body: Image(image: NetworkImage('https://zbrdst.com/assets/image/food2.png'), fit: BoxFit.cover,))));
+          body: GridView.extent(
+            maxCrossAxisExtent: 100,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 100,
+            children: [
+              Image(image: AssetImage('assets/images/1.jpg')),
+              Image(image: AssetImage('assets/images/2.jpg')),
+              Image(image: AssetImage('assets/images/3.jpg')),
+              Image(image: AssetImage('assets/images/4.jpg')),
+              Image(image: AssetImage('assets/images/5.jpg')),
+            ],
+          ))));
 }

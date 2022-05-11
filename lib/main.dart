22e1d1561219802efe41fdc5e54d0e27e6f69923
parthamandal9 <parthamandal512@gmail.com>
2022-05-11 11:02:43 +0530

    Use Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Flutter',
-    home: Container(
-      decoration: BoxDecoration(color: Colors.blueAccent),
-      child: Center(
+      title: 'Flutter',
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        body: Center(
           child: Text(
-        'Material App',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, decoration: TextDecoration.none),
-      )),
-    ),
-  ));
+            'Material App',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.none,
+                fontSize: 50),
+          ),
+        ),
+      )));
 }

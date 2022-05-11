1768e2206e55591b9f1e9dfab2cc38b1ecc223b2
parthamandal9 <parthamandal512@gmail.com>
2022-05-11 16:05:51 +0530

    Create class with StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: 'Flutter',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'Flutter App',
-              style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255), fontSize: 25),
-            ),
-            backgroundColor: Colors.black87,
-          ),
-          backgroundColor: Colors.lightBlue,
-          body: Center(
-              child: SizedBox(
-            height: 350,
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(30.0),
-                child: Column(children: [
-                  Image.asset(
-                    'assets/images/1.jpg',
-                    height: 200,
-                  ),
-                  Divider(),
-                  Text(
-                    'Nature Image',
-                    style: TextStyle(fontSize: 30),
-                  )
-                ]),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'Flutter App',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255), fontSize: 25),
               ),
+              backgroundColor: Colors.black87,
             ),
-          )))));
+            backgroundColor: Colors.lightBlue,
+            body: MyCard()));
+  }
+}
+
+class MyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: SizedBox(
+      height: 350,
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Column(children: [
+            Image.asset(
+              'assets/images/1.jpg',
+              height: 200,
+            ),
+            Divider(),
+            Text(
+              'Nature Image',
+              style: TextStyle(fontSize: 30),
+            )
+          ]),
+        ),
+      ),
+    ));
+  }
 }
